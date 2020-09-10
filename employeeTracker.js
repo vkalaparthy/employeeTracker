2736ed779d2bda2f97694b56d4c5cba107545a71
vkalaparthy <kalaparthy.vani@gmail.com>
2020-09-10 09:24:24 -0400

    Added prompt and 2 functions

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require("console.table");
+const showBanner = require("node-banner");
 
 // Open a connection
 var connection = mysql.createConnection({
@@ -22,41 +23,65 @@ connection.connect(err => {
         throw err;
     }
     console.log(`connected as id ${connection.threadId}`);
-    init();
+    (async () => {
+        await showBanner("Employee Tracker");
+        init();
+    })();
 });
 
 function init() {
-    displayDepartment();
-    // displayEmployee();
-    // displayEmpRole();
-    connection.end();
+    console.log("\n");
+    inquirer.prompt ({
+      name: "choice",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["View All employees", "View all employees by Department", 
+      "View all Departments", "Exit"]
+    })
+    .then (function(response) {
+        if (response.choice === "View All employees") {
+            viewAllEmployees();
+        }
+        else if (response.choice === "View all employees by Department") {
+            viewEmpByDept();
+        }
+        else if (response.choice === "View all Departments") {
+            viewAllDepartments();
+        }
+        else {
+            connection.end();
+        }
+    })
 };
 
-function displayDepartment() {
+function viewAllDepartments() {
     connection.query("SELECT * FROM department", function(err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
-        //console.log(res);
-        console.log("-----------------------------");
-         const table = cTable.getTable(res);
-         console.log(table);
+        console.log("\n--------------------------------------------------");
+        const table = cTable.getTable(res);
+        console.log(table);
+        // Go back to prompt user 
+        init();
     });
 };
 
-function displayEmployee() {
-    connection.query("SELECT * FROM emp_role", function(err, res) {
+function viewAllEmployees() {
+   // connection.query("SELECT * FROM employee", function(err, res) {
+    connection.query(
+    "SELECT empid, first_name, Last_name, c1.title, c1.salary, c2.name FROM employee AS c INNER JOIN emp_role AS c1 ON c.role_id = c1.id INNER JOIN department AS c2 ON c1.dept_id = c2.deptid", function(err, res) {
         if (err) throw err;
         // Log all results of the SELECT statement
-        console.log(res);
-        console.log("-----------------------------");
-      });
+        console.log("\n--------------------------------------------------");
+        const table = cTable.getTable(res);
+        console.log(table);
+        // Go back to prompt user 
+        init();
+    });
 };
 
-function displayEmpRole() {
-    connection.query("SELECT * FROM employee", function(err, res) {
-        if (err) throw err;
-        // Log all results of the SELECT statement
-        console.log(res);
-        console.log("-----------------------------");
-      });
+function viewEmpByDept() {
+    console.log("viewEmpByDept");
+    // Go back to prompt user
+    init()
 };
